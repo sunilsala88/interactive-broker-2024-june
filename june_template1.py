ba9c30a8057423bbba9370cf3a82998aeb1da289
sunilsala88 <sunilsala88@gmail.com>
2024-06-22 19:37:25 +0530

    june22

--- june_template1.py
+++ june_template1.py
@@ -30,6 +30,82 @@ def get_historical_data(ticker_contract):
     
     return df
 
+def close_ticker_postion(name,closing_price):
+
+    df2=util.df(ib.reqPositions())
+    df2['ticker_name']=[cont.symbol for cont in df2['contract']]
+    cont=df2[df2['ticker_name']==name].contract.iloc[0]
+    quant=df2[df2['ticker_name']==name].position.iloc[0]
+    quant
+    if quant>0:
+        #sell
+        ord=MarketOrder(action='SELL',totalQuantity=quant)
+        ib.placeOrder(cont,ord)
+
+
+def close_ticker_open_orders(ticker):
+    ord=ib.openTrades()
+    df1=util.df(ord)
+    print(df1)
+    print(df1.columns)
+    df1['ticker_name']=[cont.symbol for cont in df1['contract']]
+    order_object=df1[df1['ticker_name']==ticker].order.iloc[0]
+    print(order_object)
+    ib.cancelOrder(order_object)
+
+
+def trade_sell_stocks(ticker,closing_price,quantitys=1):
+    #if i have any open orders cancel all of them
+    close_ticker_open_orders(ticker)
+    #if i have current postion close current postion
+    close_ticker_postion(ticker,closing_price)
+
+def trade_buy_stocks(ticker,closing_price,quantitys=1):
+    global current_balance
+    #market order
+    contract = contract_objects[ticker]
+    ord=MarketOrder(action='BUY',totalQuantity=quantitys)
+    trade=ib.placeOrder(contract,ord)
+    ib.sleep(1)
+    print(trade)
+    #stop loss order
+
+
+    order = Order()
+    order.orderId = ib.client.getReqId()
+    order.action = 'SELL'
+    order.orderType = "STP"
+    order.totalQuantity = 1
+    order.auxPrice = int(closing_price*0.97)
+    order.tif = 'GTC'
+    trade=ib.placeOrder(contract,order)
+    ib.sleep(1)
+    print(trade)
+    
+
+
+def strategy(data,ticker):
+    global current_balance
+    print('inside strategy')
+    print(ticker)
+    print(data)
+    
+    buy_condition=data['sma1'].iloc[-1]>data['sma2'].iloc[-1] and data['sma1'].iloc[-2]<data['sma2'].iloc[-2]
+    # buy_condition=data['sma1'].iloc[-1]>data['close'].iloc[-1]
+   
+    current_balance=int(float([v for v in ib.accountValues() if v.tag == 'AvailableFunds' ][0].value))
+    
+    if current_balance>data.close.iloc[-1]:
+        if buy_condition:
+            print('buy condiiton satisfied')
+            trade_buy_stocks(ticker,data.close.iloc[-1])
+        else :
+            print('no condition satisfied')
+    else:
+        print('we dont have enough money')
+        print('current balance is',current_balance,'stock price is ',data.close[-1])
+
+
 
 def main_strategy_code():
 
@@ -60,6 +136,27 @@ def main_strategy_code():
         print(capital)
         quantity=int((capital/10)/hist_df.close.iloc[-1])  
         print(quantity)
+
+        if quantity==0:
+            print('we dont have enough money so we cannot trade')
+            continue
+
+        if pos_df.empty:
+            print('we dont have any position')
+            strategy(hist_df,ticker)
+
+
+        elif len(pos_df)!=0 and ticker not in pos_df['name'].tolist():
+            print('we have some position but current ticker is not in position')
+            strategy(hist_df,ticker)
+
+        elif len(pos_df)!=0 and ticker in pos_df["name"].tolist():
+            print('we have some position and current ticker is in position')
+            # sell_condition=hist_df['sma1'].iloc[-1]>hist_df['sma2'].iloc[-1]
+            sell_condition=hist_df['sma1'].iloc[-1]<hist_df['sma2'].iloc[-1] and hist_df['sma1'].iloc[-2]>hist_df['sma2'].iloc[-2]
+            if sell_condition:
+                print('close current ticker postion')
+                trade_sell_stocks(ticker,hist_df.close.iloc[-1])
 
 
 current_time=datetime.datetime.now()
