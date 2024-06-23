8fd8aebbb0ccd45bdcca7f62ff5e0b84409028cb
sunilsala88 <sunilsala88@gmail.com>
2024-06-23 20:02:53 +0530

    june23 8:2

--- june_template1.py
+++ june_template1.py
@@ -4,6 +4,11 @@ from ib_insync import *
 import pandas as pd
 import pandas_ta as ta
 
+
+import logging
+logging.basicConfig(level=logging.INFO, filename=f'strategy_template_{datetime.date.today()}',filemode='w',format="%(asctime)s - %(message)s")
+
+
 ib = IB()
 ib.connect('127.0.0.1', 7497, clientId=14)
 
@@ -18,7 +23,13 @@ for tick in tickers:
     contract_objects[tick]=ib.qualifyContracts(Crypto(tick,exchange,currency))[0]
 print(contract_objects)
 
+def order_open_handler(order):
+    logging.info(order)
+
+
+
 def get_historical_data(ticker_contract):
+    logging.info('fetching historical data')
     bars = ib.reqHistoricalData(
     ticker_contract, endDateTime='', durationStr='10 D',
     barSizeSetting='1 min', whatToShow='MIDPOINT', useRTH=True,formatDate=2)
@@ -27,6 +38,7 @@ def get_historical_data(ticker_contract):
     # print(df)
     df['sma1']=ta.sma(df.close,20)
     df['sma2']=ta.sma(df.close,50)
+    logging.info('calculated indicators')
     
     return df
 
@@ -41,6 +53,7 @@ def close_ticker_postion(name,closing_price):
         #sell
         ord=MarketOrder(action='SELL',totalQuantity=quant)
         ib.placeOrder(cont,ord)
+        logging.info("closing position"+'SELL'+name)
 
 
 def close_ticker_open_orders(ticker):
@@ -52,13 +65,17 @@ def close_ticker_open_orders(ticker):
     order_object=df1[df1['ticker_name']==ticker].order.iloc[0]
     print(order_object)
     ib.cancelOrder(order_object)
+    logging.info("cancelled  current order ")
+
 
 
 def trade_sell_stocks(ticker,closing_price,quantitys=1):
     #if i have any open orders cancel all of them
+    logging.info("closing all order for "+ticker)
     close_ticker_open_orders(ticker)
     #if i have current postion close current postion
     close_ticker_postion(ticker,closing_price)
+    logging.info("closing all position for "+ticker)
 
 def trade_buy_stocks(ticker,closing_price,quantitys=1):
     global current_balance
@@ -67,8 +84,9 @@ def trade_buy_stocks(ticker,closing_price,quantitys=1):
     ord=MarketOrder(action='BUY',totalQuantity=quantitys)
     trade=ib.placeOrder(contract,ord)
     ib.sleep(1)
-    print(trade)
+    logging.info(trade)
     #stop loss order
+    logging.info("placed market order")
 
 
     order = Order()
@@ -80,30 +98,31 @@ def trade_buy_stocks(ticker,closing_price,quantitys=1):
     order.tif = 'GTC'
     trade=ib.placeOrder(contract,order)
     ib.sleep(1)
-    print(trade)
+    logging.info(trade)
+    logging.info("placed stop order")
     
 
 
 def strategy(data,ticker):
     global current_balance
-    print('inside strategy')
+    logging.info('inside strategy')
     print(ticker)
     print(data)
     
     buy_condition=data['sma1'].iloc[-1]>data['sma2'].iloc[-1] and data['sma1'].iloc[-2]<data['sma2'].iloc[-2]
     # buy_condition=data['sma1'].iloc[-1]>data['close'].iloc[-1]
-   
+    buy_condition=True
     current_balance=int(float([v for v in ib.accountValues() if v.tag == 'AvailableFunds' ][0].value))
-    
+    current_balance=200000
     if current_balance>data.close.iloc[-1]:
         if buy_condition:
-            print('buy condiiton satisfied')
+            logging.info('buy condiiton satisfied')
             trade_buy_stocks(ticker,data.close.iloc[-1])
         else :
-            print('no condition satisfied')
+            logging.info('no condition satisfied')
     else:
-        print('we dont have enough money')
-        print('current balance is',current_balance,'stock price is ',data.close[-1])
+        logging.info('we dont have enough money')
+        logging.info('current balance is',current_balance,'stock price is ',data.close[-1])
 
 
 
@@ -125,8 +144,9 @@ def main_strategy_code():
         ord_df=util.df(ord)
         ord_df['name']=[cont.symbol for cont in ord_df['contract']]
     print(ord_df)
-
+    logging.info('fetched order_df and position_df')
     for ticker in tickers:
+        logging.info(ticker)
         print('ticker name is',ticker,'################')
         ticker_contract=contract_objects[ticker]
         hist_df=get_historical_data(ticker_contract)
@@ -134,53 +154,65 @@ def main_strategy_code():
         print(hist_df.close.iloc[-1])
         capital=int(float([v for v in ib.accountValues() if v.tag == 'AvailableFunds' ][0].value))
         print(capital)
-        quantity=int((capital/10)/hist_df.close.iloc[-1])  
+        capital=200000
+        quantity=int((capital)/hist_df.close.iloc[-1])  
         print(quantity)
+        logging.info('checking condition')   
 
         if quantity==0:
-            print('we dont have enough money so we cannot trade')
+            logging.info('we dont have enough money so we cannot trade')
             continue
 
         if pos_df.empty:
-            print('we dont have any position')
+            logging.info('we dont have any position')
             strategy(hist_df,ticker)
 
 
         elif len(pos_df)!=0 and ticker not in pos_df['name'].tolist():
-            print('we have some position but current ticker is not in position')
+            logging.info('we have some position but current ticker is not in position')
             strategy(hist_df,ticker)
 
         elif len(pos_df)!=0 and ticker in pos_df["name"].tolist():
-            print('we have some position and current ticker is in position')
+            logging.info('we have some position and current ticker is in position')
             # sell_condition=hist_df['sma1'].iloc[-1]>hist_df['sma2'].iloc[-1]
             sell_condition=hist_df['sma1'].iloc[-1]<hist_df['sma2'].iloc[-1] and hist_df['sma1'].iloc[-2]>hist_df['sma2'].iloc[-2]
             if sell_condition:
-                print('close current ticker postion')
+                logging.info('close current ticker postion')
                 trade_sell_stocks(ticker,hist_df.close.iloc[-1])
 
 
+
+logging.info('strategy started')
 current_time=datetime.datetime.now()
 print(current_time)
 
 print(datetime.datetime.now())
 #start time
-start_hour,start_min=18,50
+start_hour,start_min=19,48
 #end time
-end_hour,end_min=18,53
+end_hour,end_min=20,30
 start_time=datetime.datetime(current_time.year,current_time.month,current_time.day,start_hour,start_min)
 end_time=datetime.datetime(current_time.year,current_time.month,current_time.day,end_hour,end_min)
 print(start_time)
 print(end_time)
 
+logging.info('checking if start time has reached')
 while start_time>datetime.datetime.now():
     print(datetime.datetime.now())
     time.sleep(1)
 
-
+logging.info('starting the main code')
 
 candle_size=60
+#order socket
+ib.newOrderEvent += order_open_handler
+ib.orderStatusEvent += order_open_handler
+ib.cancelOrderEvent += order_open_handler
 
+logging.info('starting the main code with candle size'+str(candle_size))
 while datetime.datetime.now()<end_time:
+    # Run your function
+    main_strategy_code()
     now = datetime.datetime.now()
     print(now)
     seconds_until_next_minute = candle_size - now.second+1
@@ -188,7 +220,8 @@ while datetime.datetime.now()<end_time:
     # Sleep until the end of the current minute
     time.sleep(seconds_until_next_minute)
 
-    # Run your function
-    main_strategy_code()
 
+
+logging.info('strategy ended')
+print(datetime.datetime.now())
 print('strategy ended')
